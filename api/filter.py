260c1e054e9f1ce26e4f944ec22c99f2648ec5a3
nabil alaa <nabilalaa06@gmail.com>
2023-02-18 18:27:58 +0200

    Api-Food-Delivery

--- api/filter.py
+++
@@ -1,8 +0,0 @@
-import django_filters
-from .models import *
-
-
-class OrdersFilter(django_filters.FilterSet):
-    class Meta:
-        model = Orders
-        fields = ['name']
--- api_restaurant/settings.py
+++ api_restaurant/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$!+y14gwn8l-g+cm@il8%9n#nv825id!4yr--wv4y30g)lurs^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1:8000", "web-production-7936.up.railway.app"]
+ALLOWED_HOSTS = ["*"]
 CORS_ALLOW_ALL_ORIGINS = True
 
 
