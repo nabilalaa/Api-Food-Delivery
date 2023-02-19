db851feb22ef0333a4c8af1d863a5d3f4b8fc767
nabil alaa <nabilalaa06@gmail.com>
2023-02-19 19:55:43 +0200

    Api-Food-Delivery

--- api_restaurant/settings.py
+++ api_restaurant/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-import geoip2.database
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -44,7 +43,6 @@ INSTALLED_APPS = [
     "corsheaders",
     "django_filters",
     'rest_framework',
-    "iprestrict"
 
 ]
 
